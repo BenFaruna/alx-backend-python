5b595fbcef822b2c363972f2b7f682ba46ac957f
BenFaruna <benjaminfaruna@gmail.com>
2023-01-10 16:23:20 +0100

    Added sleep to function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,8 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator(void) -> AsyncGenerator[float, None]:
     '''function returns 10 random generated numbers'''
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
